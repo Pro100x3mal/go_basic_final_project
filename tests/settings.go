e9fa5cbb98d8d078f2e2512237d44447923300a7
Pro100x3mal <pro100x3mal@gmail.com>
2025-07-19 14:28:06 +0300

    test: set empty token by default in test settings

--- tests/settings.go
+++ tests/settings.go
@@ -4,4 +4,4 @@ var Port = 7540
 var DBFile = "../scheduler.db"
 var FullNextDate = false
 var Search = true
-var Token = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NTI5NDUxNTQsImhhc2giOiI4YzY5NzZlNWI1NDEwNDE1YmRlOTA4YmQ0ZGVlMTVkZmIxNjdhOWM4NzNmYzRiYjhhODFmNmYyYWI0NDhhOTE4In0.mbBJJLSLRWgj5kNl6BLXOOaGpbIyaULlVQo7zSnJrik`
+var Token = ``
